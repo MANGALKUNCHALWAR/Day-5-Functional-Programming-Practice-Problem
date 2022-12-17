36da5f5ffd612b38a298656f2c6ba4ac8eaab910
John Doe <mangalkunchalwar9999@gmail.com>
2022-12-17 23:49:08 +0530

    Welcome to Day 5 Functional Programming Practice

---
+++ Main.java
@@ -0,0 +1,7 @@
+package assignment5.functionalprograms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Day 5 Functional Programming Practice Problem");
+    }
+}
